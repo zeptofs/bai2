dc33f88e434485593ff6e41990911c4a2ad5d35f
Zubin Henner <9605+zubin@users.noreply.github.com>
2022-09-15 08:40:11 +1000

    Add daily_with_summary test

--- spec/bai2/bai_file_spec.rb
+++ spec/bai2/bai_file_spec.rb
@@ -2,10 +2,21 @@
 
 RSpec.describe Bai2::BaiFile do
   describe ".parse" do
-    subject(:parsed) { described_class.parse(path) }
+    subject(:parsed) { described_class.parse(path, **options) }
+
+    let(:options) { {} }
 
     context "with 'daily' fixture" do
       let(:path) { Fixtures.path("daily.bai2") }
+
+      it "instantiates without error" do
+        expect(parsed).to be_a(described_class)
+      end
+    end
+
+    context "with 'daily_with_summary' fixture" do
+      let(:path) { Fixtures.path("daily_with_summary.bai2") }
+      let(:options) { {num_account_summary_continuation_records: 3} }
 
       it "instantiates without error" do
         expect(parsed).to be_a(described_class)
