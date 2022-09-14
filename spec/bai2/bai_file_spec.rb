9de8de232bf5c3acb358e13a80c3452d534c878d
Zubin Henner <9605+zubin@users.noreply.github.com>
2022-09-15 09:08:35 +1000

    Extract shared example

--- spec/bai2/bai_file_spec.rb
+++ spec/bai2/bai_file_spec.rb
@@ -4,57 +4,52 @@ RSpec.describe Bai2::BaiFile do
   describe ".parse" do
     subject(:parsed) { described_class.parse(path, **options) }
 
-    let(:options) { {} }
-
-    context "with 'daily' fixture" do
-      let(:path) { Fixtures.path("daily.bai2") }
-
+    shared_examples "a parsable format" do
       it "instantiates without error" do
         expect(parsed).to be_a(described_class)
       end
+    end
+
+    context "with 'daily' fixture" do
+      let(:path) { Fixtures.path("daily.bai2") }
+      let(:options) { {} }
+
+      it_behaves_like "a parsable format"
     end
 
     context "with 'daily_with_summary' fixture" do
       let(:path) { Fixtures.path("daily_with_summary.bai2") }
       let(:options) { {num_account_summary_continuation_records: 3} }
 
-      it "instantiates without error" do
-        expect(parsed).to be_a(described_class)
-      end
+      it_behaves_like "a parsable format"
     end
 
     context "with 'eod' fixture" do
       let(:path) { Fixtures.path("eod.bai2") }
+      let(:options) { {} }
 
-      it "instantiates without error" do
-        expect(parsed).to be_a(described_class)
-      end
+      it_behaves_like "a parsable format"
     end
 
     context "with 'eod_without_as_of_time' fixture" do
       let(:path) { Fixtures.path("eod_without_as_of_time.bai2") }
+      let(:options) { {} }
 
-      it "instantiates without error" do
-        expect(parsed).to be_a(described_class)
-      end
+      it_behaves_like "a parsable format"
     end
 
     context "with 'eod_with_slash_in_text' fixture" do
       let(:path) { Fixtures.path("eod_with_slash_in_text.bai2") }
       let(:options) { {continuations_slash_delimit_end_of_line_only: true} }
 
-      it "instantiates without error" do
-        expect(parsed).to be_a(described_class)
-      end
+      it_behaves_like "a parsable format"
     end
 
     context "with 'group_trailer_without_number_of_accounts' fixture" do
       let(:path) { Fixtures.path("group_trailer_without_number_of_accounts.bai2") }
       let(:options) { {group_trailer_without_number_of_accounts: true} }
 
-      it "instantiates without error" do
-        expect(parsed).to be_a(described_class)
-      end
+      it_behaves_like "a parsable format"
     end
   end
 end
